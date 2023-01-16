eca3b7548f1f4cfe5185078d2e316c409985ff91
GoCoder Developer <gocoder.net@gmail.com>
2023-01-17 08:19:28 +0900

    1
    1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -282,8 +282,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0ae8021c427c8751a82a29be24681606",
-"/": "0ae8021c427c8751a82a29be24681606",
+"index.html": "64ccb0975b03939a143d7a119358e903",
+"/": "64ccb0975b03939a143d7a119358e903",
 "main.dart.js": "233c5187054a6a59485252efb91044db",
 "manifest.json": "cad8f617a45dc95dfd33919c4f259386",
 "version.json": "f5561dac955f7ebe6cff5ed419d39b22"
